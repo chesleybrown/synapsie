e86026733fd8f83ca85ea74300a2ea8191254f74
Chesley Brown <chezbrown@gmail.com>
2010-02-06 17:05:06 +0000

    - added styles for tabs in sidebar - added JS for tabs in sidebar - tweaks to facebook-auto styles - tweaks to other styles

---
+++ media/js/custom/jquery.tabs.js
@@ -0,0 +1,22 @@
+/* Should be made a simple plugin */
+
+$(document).ready(function() {
+
+	//When page loads...
+	$("div.sidebar_tab_container div.sidebar_tab_content").hide(); //Hide all content
+	$("ul.sidebar_tabs li:first").addClass("active").show(); //Activate first tab
+	$("div.sidebar_tab_container div.sidebar_tab_content:first").show(); //Show first tab content
+
+	//On Click Event
+	$("ul.sidebar_tabs li").click(function() {
+
+		$("ul.sidebar_tabs li").removeClass("active"); //Remove any "active" class
+		$(this).addClass("active"); //Add "active" class to selected tab
+		$("div.sidebar_tab_container div.sidebar_tab_content").hide(); //Hide all tab content
+
+		var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+		$(activeTab).fadeIn(); //Fade in the active ID content
+		return false;
+	});
+
+});
