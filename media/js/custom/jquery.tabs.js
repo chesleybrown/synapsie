3bc426ef2d6dffad3d108a9b1da9efa0668d8ad6
Chesley Brown <chezbrown@gmail.com>
2010-02-07 20:41:09 +0000

    - added simple check to prevent tab content from loading again if user clicked the active tab again

--- media/js/custom/jquery.tabs.js
+++ media/js/custom/jquery.tabs.js
@@ -9,13 +9,19 @@ $(document).ready(function() {
 
 	//On Click Event
 	$("ul.sidebar_tabs li").click(function() {
-
-		$("ul.sidebar_tabs li").removeClass("active"); //Remove any "active" class
-		$(this).addClass("active"); //Add "active" class to selected tab
-		$("div.sidebar_tab_container div.sidebar_tab_content").hide(); //Hide all tab content
-
-		var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
-		$(activeTab).fadeIn(); //Fade in the active ID content
+		
+		// init
+		var active_class = 'active';
+		
+		if (!$(this).hasClass(active_class)) {
+			$("ul.sidebar_tabs li").removeClass(active_class); //Remove any "active" class
+			$(this).addClass(active_class); //Add "active" class to selected tab
+			$("div.sidebar_tab_container div.sidebar_tab_content").hide(); //Hide all tab content
+	
+			var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+			$(activeTab).fadeIn(); //Fade in the active ID content
+		}
+		
 		return false;
 	});
 
