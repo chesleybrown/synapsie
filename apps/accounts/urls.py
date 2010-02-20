725d1408a8a1b6417a7f04974d7ea4c2f01626d2
Chesley Brown <chezbrown@gmail.com>
2010-02-20 14:10:02 +0000

    - fixed urls for accounts... username check should be at bottom (unless I improve the regex for it... probably still better to just have it at the bottom...? hmm...)

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,9 +3,9 @@ from taglife.apps.accounts import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.show, name='accounts_show'),
-	url(r'^(?P<user_id>\d+)$', views.show, name='accounts_show'),
-	url(r'^(?P<username>.+)$', views.show, name='accounts_show'),
 	url(r'^login/$', views.login, name='accounts_login'),
 	url(r'^logout/$', views.logout, name='accounts_logout'),
 	url(r'^register/$', views.register, name='accounts_register'),
+	url(r'^(?P<user_id>\d+)$', views.show, name='accounts_show'),
+	url(r'^(?P<username>.+)$', views.show, name='accounts_show'),
 )
