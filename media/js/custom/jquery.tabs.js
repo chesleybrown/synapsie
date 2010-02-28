aa75a60482141a6f78c43f18027ff2ec82094543
Chesley Brown <chezbrown@gmail.com>
2010-02-28 18:16:39 +0000

    - improved tab style naming convention - fixed JS for site_content tabs

--- media/js/custom/jquery.tabs.js
+++ media/js/custom/jquery.tabs.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
 
 	//When page loads...
 	$(".use_tabs").each(function() {
-		$(this).find("div.sidebar_tab_container div.sidebar_tab_content").hide(); //Hide all content
-		$(this).find("ul.sidebar_tabs li:first").addClass("active").show(); //Activate first tab
-		$(this).find("div.sidebar_tab_container div.sidebar_tab_content:first").show(); //Show first tab content
+		$(this).find("div.tab_container div.tab_content").hide(); //Hide all content
+		$(this).find("ul.tabs li:first").addClass("active").show(); //Activate first tab
+		$(this).find("div.tab_container div.tab_content:first").show(); //Show first tab content
 	});
 
 	//On Click Event
-	$(".use_tabs ul.sidebar_tabs li").click(function() {
+	$(".use_tabs ul.tabs li").click(function(e) {
 		
 		// init
 		var active_class = 'active';
@@ -19,13 +19,13 @@ $(document).ready(function() {
 		if (!$(this).hasClass(active_class)) {
 			tab_headers.find("li").removeClass(active_class); //Remove any "active" class
 			$(this).addClass(active_class); //Add "active" class to selected tab
-			tab_headers.siblings('div.sidebar_tab_container').find("div.sidebar_tab_content").hide(); //Hide all tab content
+			tab_headers.siblings('div.tab_container').find("div.tab_content").hide(); //Hide all tab content
 	
 			var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
 			$(activeTab).fadeIn(); //Fade in the active ID content
 		}
-		
-		return false;
+		console.log('called');
+		return e.preventDefault();
 	});
 
 });
