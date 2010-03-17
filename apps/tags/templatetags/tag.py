47df6c5ead3afa287ef8b1e20a369435d9cd097c
Chesley Brown <chezbrown@gmail.com>
2010-03-17 03:41:57 +0000

    - allowed to pass no tag... not really using this feature at the moment... but would like to for generating the empty tag that is used by JS...

--- apps/tags/templatetags/tag.py
+++ apps/tags/templatetags/tag.py
@@ -1,9 +1,16 @@
 from django import template
 
+from tagging.models import Tag
+
 register = template.Library()
 
 @register.inclusion_tag('tags/templatetags/tag.html')
-def render_tag(tag, close_url=False, closebutton_class=''):
+def render_tag(tag=None, close_url=False, closebutton_class=''):
+
+	# if no tag provided... generate an empty tag
+	if (tag is None):
+		tag = Tag()
+	
 	return {
 		'tag': tag,
 		'close_url': close_url,
@@ -11,7 +18,13 @@ def render_tag(tag, close_url=False, closebutton_class=''):
 	}
 
 @register.inclusion_tag('tags/templatetags/tags.html')
-def render_tags(tags, close_url=False, closebutton_class=''):
+def render_tags(tags=None, close_url=False, closebutton_class=''):
+	
+	# if no tag provided... generate an empty tag
+	if (tags is None):
+		tag = Tag()
+		tags = [tag]
+	
 	return {
 		'tags': tags,
 		'close_url': close_url,
