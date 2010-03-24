4479d1fb14b24b430edc6701bc0b208edd9c13a1
Chesley Brown <chezbrown@gmail.com>
2010-03-24 02:57:34 +0000

    - sidebar.js now updates the Popular tag section too - improved sidebar.js

--- media/js/custom/jquery.sidebar.js
+++ media/js/custom/jquery.sidebar.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Sidebar 0.4
+ * jQuery Sidebar 0.5
  * 
  * Makes it easy to handle adding/removing tags from the sidebar dynamically
  * 
@@ -28,8 +28,9 @@
 		// default settings
 		var settings = {
 			sidebar_tab_used_tags: $('#sidebar_tab_used').find('ul.tags'),
-			sidebar_tab_popular_tags: $('#sidebar_tab_popular').find('ul.tags'),
-			blank_tag: $('#tags_blank').find('li.tag')
+			sidebar_tab_popular_tags: $('#sidebar_tab_popular').find('ul.tags_chart'),
+			blank_tag: $('#tags_blank').find('li.tag'),
+			blank_popular_tag: $('#popular_tags_blank').find('li.popular_tag')
 		};
 		
 		// merge user provided options
@@ -49,15 +50,89 @@
 			var tag = settings.blank_tag.clone();
 			
 			// set tag info
-			tag.hide();
-			tag.find('a.tag_text').text(tag_name);
-			tag.find('a.tag_text').attr('href', function(index, attr) {
+			$(tag).hide();
+			$(tag).find('a.tag_text').text(tag_name);
+			$(tag).find('a.tag_text').attr('href', function(index, attr) {
 				return attr + tag_name;
 			});
-			tag.find('a.closebutton').remove();
+			$(tag).find('a.closebutton').remove();
 			
 			// tag is ready to show
 			return tag;
+			
+		}
+		
+		// build popular tag
+		function build_popular_tag(sidebar, tag_name) {
+			
+			//init
+			var settings = sidebar.data('sidebar_settings');
+			var sidebar_tab_popular_tags = settings.sidebar_tab_popular_tags;
+			var popular_tag = settings.blank_popular_tag.clone();
+			var tag = false;
+			var popular_tag_percent = 100;
+			var popular_tag_value = 1;
+			var tag_highest = 0;
+			
+			// build tag
+			tag = build_tag(sidebar, tag_name);
+			$(tag).show();
+			
+			// determine width of this tags
+			tag_highest = sidebar_tab_popular_tags.find('li:first div.content span.value').text();
+			popular_tag_percent = Math.round((parseInt(popular_tag_value) / parseInt(tag_highest)) * 100);
+			
+			// set popular tag info
+			$(popular_tag).hide();
+			$(popular_tag).find('div.label ul.tags').html(tag);
+			$(popular_tag).find('div.content span.value')
+				.css('width', popular_tag_percent + '%')
+				.text(popular_tag_value);
+			
+			// popular tag is ready to show
+			return popular_tag;
+			
+		}
+		
+		// for finding a tag container by tag_name
+		function find_tag(sidebar, tag_name) {
+			
+			//init
+			var settings = sidebar.data('sidebar_settings');
+			var sidebar_tab_used_tags = $(settings.sidebar_tab_used_tags);
+			var tag = false;
+			
+			// find tag
+			tag = $(sidebar_tab_used_tags).find('li.tag').filter(function(index) {
+				if ($(this).find('.tag_text').text() == tag_name) {
+					return true;
+				}
+				return false;
+			});
+			
+			return tag;
+			
+		}
+		
+		// for finding a popular tag container by tag_name
+		function find_popular_tag(sidebar, popular_tag_name) {
+			
+			//init
+			var settings = sidebar.data('sidebar_settings');
+			var sidebar_tab_popular_tags = $(settings.sidebar_tab_popular_tags);
+			var popular_tag_tag = false;
+			var popular_tag = false;
+			
+			// find the popular tag
+			popular_tag_tag = $(sidebar_tab_popular_tags).find('li.tag').filter(function(index) {
+				if ($(this).find('.tag_text').text() == popular_tag_name) {
+					return true;
+				}
+				return false;
+			});
+			popular_tag = $(popular_tag_tag).parents('li.popular_tag');
+			
+			return popular_tag;
 			
 		}
 		
@@ -70,7 +145,7 @@
 			var after_tag = false;
 			var already_exists = false;
 			
-			// add tag
+			// determine where to add tag
 			$(sidebar_tab_used_tags).find('li').each(function() {
 				// abort if tag already exists
 				if ($(this).find('a.tag_text').text() == $(tag).find('a.tag_text').text()) {
@@ -108,6 +183,80 @@
 			
 		}
 		
+		// insert popular tag
+		function insert_popular_tag(sidebar, popular_tag) {
+			
+			//init
+			var settings = sidebar.data('sidebar_settings');
+			var sidebar_tab_popular_tags = $(settings.sidebar_tab_popular_tags);
+			var popular_tag_name = $(popular_tag).find('div.label ul.tags .tag_text').text();
+			var tag_highest = 0;
+			var popular_tag_percent = 0;
+			var popular_tag_value = 0;
+			var current_popular_tag = false;
+			var after_tag = false;
+			var popular_tag_value = 0;
+			
+			//see if the popular tag already exists
+			$(sidebar_tab_popular_tags).find('li.popular_tag').each(function() {
+				if ($(this).find('a.tag_text').text() == $(popular_tag).find('a.tag_text').text()) {
+					current_popular_tag = $(this);
+				}
+			});
+			
+			// if tag already exists, just increase current counter
+			if ($(current_popular_tag).length > 0) {
+				
+				//increment
+				popular_tag_value = $(current_popular_tag).find('div.content span.value');
+				popular_tag_value.text(parseInt(popular_tag_value.text()) + 1);
+				
+				// determine width of this popular tag
+				tag_highest = sidebar_tab_popular_tags.find('li:first div.content span.value').text();
+				popular_tag_percent = Math.round((parseInt($(popular_tag_value).text()) / parseInt(tag_highest)) * 100);
+				
+				// set popular tag info
+				$(popular_tag_value).css('width', popular_tag_percent + '%');
+				
+				// forgot the popular tag, we are just going to move around the current one
+				popular_tag = current_popular_tag;
+				
+			}
+			
+			// get the current popular tag value
+			popular_tag_value = parseInt($(popular_tag).find('div.content span.value').text());
+			
+			// determine where to add popular tag
+			$(sidebar_tab_popular_tags).find('li.popular_tag').each(function() {
+				var current_popular_tag_value = parseInt($(this).find('div.content span.value').text());
+				
+				if (current_popular_tag_value > popular_tag_value) {
+					after_tag = $(this);
+				}
+			});
+			
+			// can go after a current tag
+			if (after_tag) {
+				$(after_tag).after(popular_tag);
+			}
+			
+			// can't go after anything... must be first
+			else if ($(sidebar_tab_popular_tags).children('li.popular_tag').length > 0) {
+				$(sidebar_tab_popular_tags).prepend(popular_tag);
+			}
+			
+			// nothing it can go after, just append
+			else {
+				$(sidebar_tab_popular_tags).append(popular_tag);
+			}
+			
+			// fade it all fancy
+			$(popular_tag).fadeIn('slow');
+			
+			return true;
+			
+		}
+		
 		// add new tag (will not add it if it already exists)
 		function add_tag(sidebar, tag_name) {
 			
@@ -116,19 +265,17 @@
 			var sidebar_tab_used_tags = $(settings.sidebar_tab_used_tags);
 			var sidebar_tab_popular_tags = $(settings.sidebar_tab_popular_tags);
 			var tag = false;
+			var popular_tag = false;
 			
 			// build the tag
 			tag = build_tag(sidebar, tag_name);
 			
-			// only add tag if it isn't already there.
-			if ($(sidebar_tab_used_tags).find('li.tag .tag_text:contains("' + tag.text() + '")').length > 0) {
-				return false;
-			}
+			// build the popular tag container
+			popular_tag = build_popular_tag(sidebar, tag_name);
 			
 			// add tag
 			insert_tag(sidebar, tag);
-			
-			//sidebar_tab_popular_tags.append(tag);
+			insert_popular_tag(sidebar, popular_tag);
 			
 			return true;
 			
@@ -142,14 +289,15 @@
 			var sidebar_tab_used_tags = $(settings.sidebar_tab_used_tags);
 			var sidebar_tab_popular_tags = $(settings.sidebar_tab_popular_tags);
 			var tag = false;
+			var new_tag = false;
+			var popular_tag_tag = false;
+			var popular_tag = false;
 			
 			// find the tag
-			tag = $(sidebar_tab_used_tags).find('li.tag').filter(function(index) {
-				if ($(this).find('.tag_text').text() == old_tag_name) {
-					return true;
-				}
-				return false;
-			});
+			tag = find_tag(sidebar, old_tag_name);
+			
+			// find the popular tag
+			popular_tag = find_popular_tag(sidebar, old_tag_name);
 			
 			// update the tag
 			new_tag = $(tag).clone();
@@ -158,11 +306,20 @@
 				return attr.replace(/(\/tags\/).*/, '$1' + new_tag_name);
 			});
 			
+			// update the popular tag
+			new_popular_tag = $(popular_tag).clone();
+			new_popular_tag.find('a.tag_text').text(new_tag_name);
+			new_popular_tag.find('a.tag_text').attr('href', function(index, attr) {
+				return attr.replace(/(\/tags\/).*/, '$1' + new_tag_name);
+			});
+			
 			// remove old tag
 			$(tag).remove();
+			$(popular_tag).remove();
 			
 			// update location of tag (sort)
 			insert_tag(sidebar, new_tag);
+			insert_popular_tag(sidebar, new_popular_tag);
 			
 			return true;
 			
@@ -176,21 +333,23 @@
 			var sidebar_tab_used_tags = $(settings.sidebar_tab_used_tags);
 			var sidebar_tab_popular_tags = $(settings.sidebar_tab_popular_tags);
 			var tag = false;
+			var popular_tag = false;
 			
 			// find the tag
-			tag = $(sidebar_tab_used_tags).find('li.tag').filter(function(index) {
-				if ($(this).find('.tag_text').text() == tag_name) {
-					return true;
-				}
-				return false;
-			});
+			tag = find_tag(sidebar, tag_name);
+			
+			// find the popular tag
+			popular_tag = find_popular_tag(sidebar, tag_name);
 			
 			// remove the tag
-			if (tag.length > 0) {
-				tag.fadeOut('slow', function() {
-					$(this).remove();
-				});
-			}
+			$(tag).fadeOut('slow', function() {
+				$(this).remove();
+			});
+			
+			// remove the popular tag
+			$(popular_tag).fadeOut('slow', function() {
+				$(this).remove();
+			});
 			
 			return true;
 			
