1823293ee48b1d20e5e7452db8fa87984ea51761
Chesley Brown <chezbrown@gmail.com>
2010-10-20 02:28:40 +0000

    - changed email address to be noreply@synapsie.com

--- settings.py
+++ settings.py
@@ -132,7 +132,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(PROJECT_ROOT, 'logs', 'emails')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'admin@synapsie.com'
+EMAIL_HOST_USER = 'noreply@synapsie.com'
 EMAIL_HOST_PASSWORD = 'abozv8x9d8bj'
 EMAIL_USE_TLS = True
 
