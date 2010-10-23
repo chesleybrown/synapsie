90d1b1abaf61b9230bfd3d3c5a0391d3e597e020
Chesley Brown <chezbrown@gmail.com>
2010-10-23 00:13:41 +0000

    - created the page that users hitting the site with IE will see... just need to put the redirect logic in there now

--- apps/about/urls.py
+++ apps/about/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 from apps.about import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns('django.views.generic.simple',
 	url(r'^$', views.home, name='home'),
 	url(r'^about/$', views.about, name="about"),
 	url(r'^terms/$', views.terms, name="terms"),
 	url(r'^privacy/$', views.privacy, name="privacy"),
 	url(r'^acceptableuse/$', views.acceptableuse, name="acceptableuse"),
 	url(r'^copyright/$', views.copyright, name="copyright"),
+	url(r'^browsers/$', 'direct_to_template', {'template': 'about/browsers.html'}),
 )
