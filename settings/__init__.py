0a62f6b171b89cb8614471c665dd97abf267edcb
Chesley Brown <chezbrown@gmail.com>
2010-11-02 12:23:41 +0000

    - trying middleware instead of middlewares again

--- settings/__init__.py
+++ settings/__init__.py
@@ -98,7 +98,7 @@ MIDDLEWARE_CLASSES = (
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.transaction.TransactionMiddleware',
-	'middlewares.ssl.SSLRedirect',
+	'middleware.ssl.SSLRedirect',
 )
 
 ROOT_URLCONF = 'urls'
