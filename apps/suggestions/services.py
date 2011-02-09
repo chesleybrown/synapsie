8ffd5dded71170245d73448f8f60aed3229610f9
Chesley Brown <chezbrown@gmail.com>
2011-02-09 17:54:47 -0330

    Created a much faster way of randomly selecting a suggestion to display to the user (MySQL random order sucked too much)

--- apps/suggestions/services.py
+++ apps/suggestions/services.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import random
 
 from django.db.models import Q
 
@@ -30,10 +31,19 @@ def get_next_suggestion(request, user=None):
 		user_suggestion_ids.append(user_suggestion.id)
 	
 	# get a random suggestion that the user hasn't already completed or ignored
+	# we have to determine a random suggestion to select like this because MySQL random order sucks
+	num_suggestions = apps.suggestions.models.Suggestion.objects.count()
+	random_id_range = random.randint(1, num_suggestions)
+	if random.random() > 0.5:
+		random_sort = 'id'
+	else:
+		random_sort = '-id'
+	
 	suggestions = (
 		apps.suggestions.models.Suggestion.objects
 		.exclude(pk__in=user_suggestion_ids)
-		.order_by('?')[:1]
+		.filter(pk__gte=random_id_range)
+		.order_by(random_sort)[:1]
 	)
 	
 	# get the first suggestion
