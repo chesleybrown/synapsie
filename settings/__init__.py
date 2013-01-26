61ef70972050393eb5c00b74417895bbc6c1f3da
Chesley Brown <chezbrown@gmail.com>
2013-01-26 18:24:01 -0330

    Fixed prod db settings

--- settings/__init__.py
+++ settings/__init__.py
@@ -21,8 +21,8 @@ DATABASES = {
 		'NAME': 'dj_synapsie',						# Or path to database file if using sqlite3.
 		'USER': 'dj_synapsie',						# Not used with sqlite3.
 		'PASSWORD': 'syn.apsie.141',					# Not used with sqlite3.
-		'HOST': '127.0.0.1',					  # Set to empty string for localhost. Not used with sqlite3.
-		'PORT': '3308',						 # Set to empty string for default. Not used with sqlite3.
+		'HOST': 'localhost',					  # Set to empty string for localhost. Not used with sqlite3.
+		'PORT': '3306',						 # Set to empty string for default. Not used with sqlite3.
 	}
 }
 
